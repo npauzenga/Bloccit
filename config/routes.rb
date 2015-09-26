4b0b43c83e9bf81649ec9f2270c228930258c446
Nate Pauzenga <npauzenga@gmail.com>
2015-09-26 09:51:23 -0400

    added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   # implied hash, could be written root({to: 'welcome#index'})
   root to: 'welcome#index'
 end
