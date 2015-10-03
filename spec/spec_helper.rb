91c37ee075a3742bb05434cfcf587a2ea5f0e6c4
Nate Pauzenga <npauzenga@gmail.com>
2015-10-02 20:06:22 -0400

    reconfigured spec_helper for coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require "coveralls"
-Coveralls.wear!
-
+require "rspec"
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -46,7 +44,7 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
@@ -92,5 +90,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
