83d556a7e9940cdefb4bb95a09e3d5e204c0fee0
Nate Pauzenga <npauzenga@gmail.com>
2015-10-04 12:21:40 -0400

    removed redundant method

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,3 @@
 class Question < ActiveRecord::Base
   has_many :answers
-
-
-    def display_resolved
-      if @question.resolved
-        "This question has been resolved."
-      else
-        "This question needs an answer!"
-      end
-    end
 end
