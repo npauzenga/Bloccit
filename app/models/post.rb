e2ed411deede089cf226f7eebce71ce101b34929
Nate Pauzenga <npauzenga@gmail.com>
2015-10-03 20:39:21 -0400

    completed assignment 42

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order("created_at DESC") }
+  default_scope { order(created_at: :desc) }
+  scope :ordered_by_title, -> { reorder(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
 end
