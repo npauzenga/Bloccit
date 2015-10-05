a28c4ac2b8e5fa01bbf4782ef053c215a2df7c89
Nate Pauzenga <npauzenga@gmail.com>
2015-10-04 21:58:54 -0400

    halfway through checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,20 @@ require 'faker'
 end
 users = User.all
 
+# Create topics
+15.times do
+  Topic.create!(
+    name:        Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 # create posts
 50.times do
   Post.create!(
     user:   users.sample,
+    topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
