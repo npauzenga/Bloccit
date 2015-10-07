d40c76a1b749fa94fe5d71d54e7d7830ac62c59c
Nate Pauzenga <npauzenga@gmail.com>
2015-10-06 22:27:27 -0400

    create not quite working but path correct

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,18 +1,19 @@
 class SummariesController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    @summary = topic_post_summary_path # kind of a bandaid
-    # authorize @summary # broken due to above hackery
+    @summary = Summary.new
+    authorize @summary
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @summary = Summary.new(summary_params)
     @post.summary = @summary
     authorize @summary
     if @summary.save
       flash[:notice] = "Summary was saved."
-      redirect_to [@topic, @post, @summary] # nope
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the Summary. Please try again."
       render :new
