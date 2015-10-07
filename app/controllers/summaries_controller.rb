26f44293b970063c5c03b6eca48d0790d7c02586
Nate Pauzenga <npauzenga@gmail.com>
2015-10-06 21:58:53 -0400

    filled in authorizations

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,15 +1,22 @@
 class SummariesController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    @summary = topic_post_summary_path
-    #authorize @summary
+    @summary = topic_post_summary_path # kind of a bandaid
+    # authorize @summary # broken due to above hackery
   end
 
   def create
     @post = Post.find(params[:post_id])
     @summary = Summary.new(summary_params)
     @post.summary = @summary
-    # Authorize and redirect
+    authorize @summary
+    if @summary.save
+      flash[:notice] = "Summary was saved."
+      redirect_to [@topic, @post, @summary] # nope
+    else
+      flash[:error] = "There was an error saving the Summary. Please try again."
+      render :new
+    end
   end
 
   def show
