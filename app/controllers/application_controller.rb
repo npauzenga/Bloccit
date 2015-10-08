ad98089840e4434daaaa014d3a88a2e4a840b91d
Nate Pauzenga <npauzenga@gmail.com>
2015-10-07 21:27:15 -0400

    completed checkpoint 46 Validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :flash_attack
-
-  def flash_attack
-    flash[:notice] = "Flash!"
-  end
 
   rescue_from Pundit::NotAuthorizedError do |exception|
     redirect_to root_url, alert: exception.message
