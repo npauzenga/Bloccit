a8619ea641532e368a858c867812e7d40dd3ec36
Nate Pauzenga <npauzenga@gmail.com>
2015-10-11 16:18:30 -0400

    working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
+    @comment = Comment.new
     authorize @post
   end
 
