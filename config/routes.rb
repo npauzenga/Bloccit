7d595f39bac294952749031312e1e74c1d06021f
Nate Pauzenga <npauzenga@gmail.com>
2015-10-11 11:56:08 -0400

    added comment associations and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
 
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create]
+    end
   end
 
   get 'about' => 'welcome#about'
