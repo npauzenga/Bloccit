d56b9978c947ca65f1d5aafe58e7f5d4754589ad
Nate Pauzenga <npauzenga@gmail.com>
2015-10-13 16:38:14 -0400

    initialized @vote instance for each test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,9 +2,13 @@ require "rails_helper"
 
 describe Vote do
   describe "validations" do
+
+    let(:vote) { Vote.create(value: 1) }
+
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(@vote.save).to eq(true)
+        vote.value = 2
+        expect(vote.save).to eq(false)
       end
     end
   end
