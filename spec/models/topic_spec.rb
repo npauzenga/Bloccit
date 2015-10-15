cd715b7cb4ed25fe61bb3411c5e1ab95b3d2f612
Nate Pauzenga <npauzenga@gmail.com>
2015-10-15 18:53:50 -0400

    completed assignment 52

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,36 @@
 require "rails_helper"
 
-RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Topic do
+   describe "scopes" do
+
+       let(:public_topic) { Topic.create(name: "Topic Name",
+                                         description: "Topic description") } # default is public
+       let(:private_topic) { Topic.create(name: "Topic Name",
+                                          description: "Topic description",
+                                          public: false) }
+
+     describe "publicly_viewable" do
+       it "returns a relation of all public topics" do
+         expect(described_class.publicly_viewable).to eq( [public_topic] )
+       end
+     end
+
+     describe "privately_viewable" do
+       it "returns a relation of all private topics" do
+         expect(described_class.privately_viewable).to eq( [private_topic] )
+       end
+     end
+
+     describe "visible_to(user)" do
+       it "returns all topics if the user is present" do
+         user = User.new
+         expect(Topic.visible_to(user)).to eq( described_class.all )
+       end
+
+       it "returns only public topics if user is nil" do
+         user = nil
+         expect(Topic.visible_to(user)).to eq( [public_topic] )
+       end
+     end
+   end
+ end
