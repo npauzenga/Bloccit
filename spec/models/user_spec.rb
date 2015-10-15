25a1d03a7b9a8a2728a4909dfd4c214a65476727
Nate Pauzenga <npauzenga@gmail.com>
2015-10-15 13:15:01 -0400

    completed checkpoint 55

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe User do
+  include TestFactories
+
+  describe "#favorited(post)" do
+    xit "returns 'nil' if the user has not favorited the post" do
+    end
+
+    xit "retursn the appropriate favorite if it exists" do
+    end
+  end
+end
