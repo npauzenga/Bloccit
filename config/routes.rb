1344060a0040d09dcc1c93dc2b50812d9a13966e
Nate Pauzenga <npauzenga@gmail.com>
2015-10-17 20:56:31 -0400

    almost there, spec not passing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :posts, :questions
   resources :advertisements, only: [:index, :show]
   devise_for :users
-  resources :users, only: [:update]
+  resources :users, only: [:update, :show]
 
   resources :topics do
     resources :posts, except: [:index]
