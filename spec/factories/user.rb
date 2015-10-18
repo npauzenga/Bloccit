c452c3184d77a343c8c0e7cf97064f9651f2429f
Nate Pauzenga <npauzenga@gmail.com>
2015-10-18 16:59:22 -0400

    working through assignment 56

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,15 @@
 FactoryGirl.define do
   factory :user do
     name "Douglas Adams"
-    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    sequence(:email, 200) { |n| "person#{n}@example.com" }
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+
+    factory :user_with_post_and_comment do
+      after(:build) { create(:post) }
+      after(:build) { create(:comment) }
+    end
   end
 end
